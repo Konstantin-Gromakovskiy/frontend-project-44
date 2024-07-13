535f925d31818b64539c606b0b06aea6a3e5c2af
Gromakovskiy <k,gromakovskiy@yandex.ru>
2024-07-13 13:00:27 +0300

    debugging src/games/prime.js

--- src/games/prime.js
+++ src/games/prime.js
@@ -3,7 +3,7 @@ import game from '../index.js';
 const instruction = 'Answer "yes" if given number is prime. Otherwise answer "no".';
 
 const getQuestionAndAnswer = () => {
-  const randomNumber = Math.floor(Math.random() * 100) + 1;
+  const randomNumber = Math.floor(Math.random() * 99) + 2;
   let answer = 'yes';
   for (let i = 2; i < randomNumber; i += 1) {
     if (randomNumber % i === 0) {
