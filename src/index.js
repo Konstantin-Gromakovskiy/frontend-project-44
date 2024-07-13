d6ba04692d7882caf938bacd5eab81d8834ee411
Gromakovskiy <k,gromakovskiy@yandex.ru>
2024-07-13 12:05:42 +0300

    debugging

--- src/index.js
+++ src/index.js
@@ -16,7 +16,7 @@ export default (instruction, getQuestionAndAnswer) => {
       console.log('Correct!');
     } else {
       console.log(`'${userAnswer}' is wrong answer ;(. Correct answer was '${answer}'.`);
-      console.log(`Let's try again, ${name}`);
+      console.log(`Let's try again, ${name}!`);
       break;
     }
 
